ee7c54ee1d4d7ecb03422aa14e52b612b3989789
TechVio <68242352+techvio1@users.noreply.github.com>
2021-08-02 01:57:23 +0100

    PyCalc 5.2 Release

--- pycalc.py
+++ pycalc.py
@@ -1,26 +1,24 @@
 import sys
 import time
 
-decore = "==============================="
+decore = "-------------------------------"
 byetext = "\n\nThank you For Using Our Program\n\nExiting..."
 print(decore)
 print("\nWelcome To PyCalc Project: CLI Edition\n")
 print(decore)
 time.sleep(0.2)
-print("\nVersion:5.1\n")
+print("\nVersion:5.2\n")
 print("Branch: Unstable")
 print(decore)
 print("Copyright © 2021 TechVio")
 time.sleep(0.5)
 print(decore)
-print("Project Link: https://www.github.com/techvio1/PyCalc-Project/")
-print(decore)
-time.sleep(1)
-print("License: MIT License: https://mit-license.org/")
+print("Licensed under MIT License: https://mit-license.org/")
 print(decore)
 time.sleep(0.2)
-print("running...")
-
+print("Starting The Console...")
+print(decore)
+time.sleep(1)
 
 def calc():
     time.sleep(1.0)
@@ -44,7 +42,9 @@ def calc():
             time.sleep(1.0)
             calc()
         elif request == 'n':
+            print(decore)
             print("Back To Console")
+            print(decore)
             time.sleep(1)
             console()
         elif request == 'q':
@@ -53,7 +53,9 @@ def calc():
             time.sleep(0.5)
             sys.exit()
         else:
+            print(decore)
             print("Incorrect Parameter, back to console")
+            print(decore)
             time.sleep(1.5)
             console()
     elif operation == "+":
@@ -66,7 +68,9 @@ def calc():
             time.sleep(1.0)
             calc()
         elif request == 'n':
+            print(decore)
             print("Back To Console")
+            print(decore)
             time.sleep(1)
             console()
         elif request == 'q':
@@ -75,7 +79,9 @@ def calc():
             time.sleep(0.5)
             sys.exit()
         else:
+            print(decore)
             print("Incorrect Parameter, back to console")
+            print(decore)
             time.sleep(1.5)
             console()
     elif operation == "-":
@@ -88,7 +94,9 @@ def calc():
             time.sleep(1.0)
             calc()
         elif request == 'n':
+            print(decore)
             print("Back To Console")
+            print(decore)
             time.sleep(1)
             console()
         elif request == 'q':
@@ -97,7 +105,9 @@ def calc():
             time.sleep(0.5)
             sys.exit()
         else:
+            print(decore)
             print("Incorrect Parameter, back to console")
+            print(decore)
             time.sleep(1.5)
             console()
     elif operation == "/":
@@ -110,7 +120,9 @@ def calc():
             time.sleep(1.0)
             calc()
         elif request == 'n':
+            print(decore)
             print("Back To Console")
+            print(decore)
             time.sleep(1)
             console()
         elif request == 'q':
@@ -119,7 +131,9 @@ def calc():
             time.sleep(0.5)
             sys.exit()
         else:
+            print(decore)
             print("Incorrect Parameter, back to console")
+            print(decore)
             time.sleep(1.5)
             console()
     elif operation == "%":
@@ -132,7 +146,9 @@ def calc():
             time.sleep(1.0)
             calc()
         elif request == 'n':
+            print(decore)
             print("Back To Console")
+            print(decore)
             time.sleep(1)
             console()
         elif request == 'q':
@@ -141,7 +157,9 @@ def calc():
             time.sleep(0.5)
             sys.exit()
         else:
+            print(decore)
             print("Incorrect Parameter, back to console")
+            print(decore)
             time.sleep(1.5)
             console()
     elif operation == "**":
@@ -154,7 +172,9 @@ def calc():
             time.sleep(1.0)
             calc()
         elif request == 'n':
+            print(decore)
             print("Back To Console")
+            print(decore)
             time.sleep(1)
             console()
         elif request == 'q':
@@ -163,7 +183,9 @@ def calc():
             time.sleep(0.5)
             sys.exit()
         else:
+            print(decore)
             print("Incorrect Parameter, back to console")
+            print(decore)
             time.sleep(1.5)
             console()
     elif operation == "//":
@@ -176,7 +198,9 @@ def calc():
             time.sleep(1.0)
             calc()
         elif request == 'n':
+            print(decore)
             print("Back To Console")
+            print(decore)
             time.sleep(1)
             console()
         elif request == 'q':
@@ -185,27 +209,31 @@ def calc():
             time.sleep(0.5)
             sys.exit()
         else:
+            print(decore)
             print("Incorrect Parameter, back to console")
+            print(decore)
             time.sleep(1.5)
             console()
     else:
         time.sleep(0.5)
+        print(decore)
         print("Error: Incorrect Operation:")
+        print(decore)
         time.sleep(0.5)
         print("=====================")
-        print("Addition:         +")
+        print("Addition       :   +")
         print("=====================")
-        print("Subtraction:      -")
+        print("Subtraction    :   -")
         print("=====================")
-        print("Multiplication:   *")
+        print("Multiplication :   *")
         print("=====================")
-        print("Division:         /")
+        print("Division       :   /")
         print("=====================")
-        print("Modulus:          %")
+        print("Modulus        :   %")
         print("=====================")
-        print("Exponentiation:   **")
+        print("Exponentiation :   **")
         print("=====================")
-        print("Floor division:   //")
+        print("Floor division :   //")
         print("=====================")
         time.sleep(1.0)
         calc()
@@ -213,13 +241,19 @@ def calc():
 
 def squareroot():
     from math import sqrt
+    print(decore)
     num = float(input("Enter A Number: "))
+    print(decore)
     result = sqrt(num)
     print(result)
+    print(decore)
     request = input("Do you want to do another square root operation ? (y/n/q): ")
     if request == 'y':
         squareroot()
     elif request == 'n':
+        print(decore)
+        print("Back to console")
+        print(decore)
         console()
     elif request == 'q':
         time.sleep(0.5)
@@ -227,7 +261,9 @@ def squareroot():
         time.sleep(0.5)
         sys.exit()
     else:
+        print(decore)
         print("Incorrect Parameter, back to console")
+        print(decore)
         time.sleep(1.5)
         console()
 
@@ -236,9 +272,10 @@ def console():
     calculator_mode = input("Select Calculator Mode: ")
     if calculator_mode == "help":
         print(decore)
-        print('square root mode: "sqrt"')
-        print('normal Calculator Mode: "calc"')
-        print('console version: "version"')
+        print('square root mode      : "sqrt"        ')
+        print('normal Calculator Mode: "calc"        ')
+        print('console version       : "version"     ')
+        print('Project Links         : "projectlinks"')
         print(decore)
         console()
     elif calculator_mode == "calc":
@@ -246,13 +283,18 @@ def console():
     elif calculator_mode == "sqrt":
         squareroot()
     elif calculator_mode == "version":
-        print("pycalc console v1.0 build 062621")
+        print("pycalc console v1.2 build 080221")
         console()
     elif calculator_mode == "exit":
         time.sleep(0.5)
         print(byetext)
         time.sleep(0.5)
         sys.exit()
+    elif calculator_mode == "projectlinks":
+        print("Github: https://www.github.com/techvio1/PyCalc-Project/")
+        print("GitLab: Working on it")
+        console()
+
     else:
         print(decore)
         print("Incorrect mode: Type 'help' for more information")
