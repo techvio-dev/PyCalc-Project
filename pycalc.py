7695b19863d6f6e525c611968f9e17db6040394e
TechVio <68242352+techvio1@users.noreply.github.com>
2022-01-01 02:43:35 +0100

    PyCalc 6.0 release

--- pycalc.py
+++ pycalc.py
@@ -1,258 +1,184 @@
 import sys
-import time
-
-decore = "==============================="
+decore = "-------------------------------"
 byetext = "\n\nThank you For Using Our Program\n\nExiting..."
 print(decore)
-print("\nWelcome To PyCalc Project: CLI Edition\n")
+print("\nWelcome to PyCalc project: CLI Edition\n")
 print(decore)
-time.sleep(0.2)
-print("\nVersion:5.1\n")
-print("Branch: Bêta")
+print("\nVersion:6.0\n")
+print("Branch: Beta")
 print(decore)
-print("Copyright © 2021 TechVio")
-time.sleep(0.5)
+print("Copyright © 2022 TechVio")
 print(decore)
-print("Project Link: https://www.github.com/techvio1/PyCalc-Project/")
+print("Licensed under MIT license: https://mit-license.org/")
 print(decore)
-time.sleep(1)
-print("License: MIT License: https://mit-license.org/")
+print("Starting the console...")
 print(decore)
-time.sleep(0.2)
-print("running...")
+
+
+def calcreq():
+    request = input("Do you want to do another operation (y/n/q): ")
+    if request == "y":
+        print("please wait...")
+        calc()
+    elif request == 'n':
+        print(decore)
+        print("Back to console")
+        print(decore)
+        console()
+    elif request == 'q':
+        print(byetext)
+        sys.exit()
+    else:
+        print(decore)
+        print("Incorrect Parameter, back to console")
+        print(decore)
+        console()
+
+
+def mcalcreq():
+    request = input("Do you want to do another operation (y/n/q): ")
+    if request == "y":
+        print("Please Wait...")
+        mcalc()
+    elif request == 'n':
+        print(decore)
+        print("Back to console")
+        print(decore)
+        console()
+    elif request == 'q':
+        print(byetext)
+        sys.exit()
+    else:
+        print(decore)
+        print("Incorrect parameter, back to console")
+        print(decore)
+        console()
+
+
+def sqrtreq():
+    request = input("Do you want to do another operation (y/n/q): ")
+    if request == "y":
+        print("Please wait...")
+        sqrt()
+    elif request == 'n':
+        print(decore)
+        print("Back to console")
+        print(decore)
+        console()
+    elif request == 'q':
+        print(byetext)
+        sys.exit()
+    else:
+        print(decore)
+        print("Incorrect parameter, back to console")
+        print(decore)
+        console()
 
 
 def calc():
-    time.sleep(1.0)
+
     print(decore)
     num1 = float(input("Enter Your First Number: "))
     print(decore)
-    time.sleep(0.1)
     operation = input("Enter Your Operation: ")
     print(decore)
-    time.sleep(0.1)
     num2 = float(input("Enter Your Second Number:"))
     print(decore)
-    time.sleep(0.1)
     if operation == "*":
         result = float(num1 * num2)
         print(result)
-        time.sleep(0.5)
-        request = input("Do you want to do another operation (y/n/q): ")
-        if request == "y":
-            print("Please Wait...")
-            time.sleep(1.0)
-            calc()
-        elif request == 'n':
-            print("Back To Console")
-            time.sleep(1)
-            console()
-        elif request == 'q':
-            time.sleep(0.5)
-            print(byetext)
-            time.sleep(0.5)
-            sys.exit()
-        else:
-            print("Incorrect Parameter, back to console")
-            time.sleep(1.5)
-            console()
+        calcreq()
     elif operation == "+":
         result = float(num1 + num2)
         print(result)
-        time.sleep(0.5)
-        request = input("Do you want to do another operation (y/n/q): ")
-        if request == "y":
-            print("Please Wait...")
-            time.sleep(1.0)
-            calc()
-        elif request == 'n':
-            print("Back To Console")
-            time.sleep(1)
-            console()
-        elif request == 'q':
-            time.sleep(0.5)
-            print(byetext)
-            time.sleep(0.5)
-            sys.exit()
-        else:
-            print("Incorrect Parameter, back to console")
-            time.sleep(1.5)
-            console()
+        calcreq()
     elif operation == "-":
         result = float(num1 - num2)
         print(result)
-        time.sleep(0.5)
-        request = input("Do you want to do another operation (y/n/q): ")
-        if request == "y":
-            print("Please Wait...")
-            time.sleep(1.0)
-            calc()
-        elif request == 'n':
-            print("Back To Console")
-            time.sleep(1)
-            console()
-        elif request == 'q':
-            time.sleep(0.5)
-            print(byetext)
-            time.sleep(0.5)
-            sys.exit()
-        else:
-            print("Incorrect Parameter, back to console")
-            time.sleep(1.5)
-            console()
+        calcreq()
     elif operation == "/":
         result = float(num1 / num2)
         print(result)
-        time.sleep(0.5)
-        request = input("Do you want to do another operation (y/n/q): ")
-        if request == "y":
-            print("Please Wait...")
-            time.sleep(1.0)
-            calc()
-        elif request == 'n':
-            print("Back To Console")
-            time.sleep(1)
-            console()
-        elif request == 'q':
-            time.sleep(0.5)
-            print(byetext)
-            time.sleep(0.5)
-            sys.exit()
-        else:
-            print("Incorrect Parameter, back to console")
-            time.sleep(1.5)
-            console()
+        calcreq()
     elif operation == "%":
         result = float(num1 % num2)
         print(result)
-        time.sleep(0.5)
-        request = input("Do you want to do another operation (y/n/q): ")
-        if request == "y":
-            print("Please Wait...")
-            time.sleep(1.0)
-            calc()
-        elif request == 'n':
-            print("Back To Console")
-            time.sleep(1)
-            console()
-        elif request == 'q':
-            time.sleep(0.5)
-            print(byetext)
-            time.sleep(0.5)
-            sys.exit()
-        else:
-            print("Incorrect Parameter, back to console")
-            time.sleep(1.5)
-            console()
+        calcreq()
     elif operation == "**":
         result = float(num1 ** num2)
         print(result)
-        time.sleep(0.5)
-        request = input("Do you want to do another operation (y/n/q): ")
-        if request == "y":
-            print("Please Wait...")
-            time.sleep(1.0)
-            calc()
-        elif request == 'n':
-            print("Back To Console")
-            time.sleep(1)
-            console()
-        elif request == 'q':
-            time.sleep(0.5)
-            print(byetext)
-            time.sleep(0.5)
-            sys.exit()
-        else:
-            print("Incorrect Parameter, back to console")
-            time.sleep(1.5)
-            console()
+        calcreq()
     elif operation == "//":
         result = float(num1 // num2)
         print(result)
-        time.sleep(0.5)
-        request = input("Do you want to do another operation (y/n/q): ")
-        if request == "y":
-            print("Please Wait...")
-            time.sleep(1.0)
-            calc()
-        elif request == 'n':
-            print("Back To Console")
-            time.sleep(1)
-            console()
-        elif request == 'q':
-            time.sleep(0.5)
-            print(byetext)
-            time.sleep(0.5)
-            sys.exit()
-        else:
-            print("Incorrect Parameter, back to console")
-            time.sleep(1.5)
-            console()
+        calcreq()
     else:
-        time.sleep(0.5)
+        print(decore)
         print("Error: Incorrect Operation:")
-        time.sleep(0.5)
-        print("=====================")
-        print("Addition:         +")
-        print("=====================")
-        print("Subtraction:      -")
-        print("=====================")
-        print("Multiplication:   *")
-        print("=====================")
-        print("Division:         /")
-        print("=====================")
-        print("Modulus:          %")
-        print("=====================")
-        print("Exponentiation:   **")
-        print("=====================")
-        print("Floor division:   //")
-        print("=====================")
-        time.sleep(1.0)
+        print(decore)
+        print(decore)
+        print("Addition      : +")
+        print(decore)
+        print("Subtraction   : -")
+        print(decore)
+        print("Multiplication: *")
+        print(decore)
+        print("Division      : /")
+        print(decore)
+        print("Modulus       : %")
+        print(decore)
+        print("Exponentiation: **")
+        print(decore)
+        print("Floor division: //")
+        print(decore)
         calc()
 
 
-def squareroot():
-    from math import sqrt
+def sqrt():
+    from math import sqrt as sqrtmath
+    print(decore)
     num = float(input("Enter A Number: "))
-    result = sqrt(num)
+    print(decore)
+    result = sqrtmath(num)
     print(result)
-    request = input("Do you want to do another square root operation ? (y/n/q): ")
-    if request == 'y':
-        squareroot()
-    elif request == 'n':
-        console()
-    elif request == 'q':
-        time.sleep(0.5)
-        print(byetext)
-        time.sleep(0.5)
-        sys.exit()
-    else:
-        print("Incorrect Parameter, back to console")
-        time.sleep(1.5)
-        console()
+    print(decore)
+    sqrtreq()
+
+
+def mcalc():
+    calc = input("Type calculation: ")
+    print(str(eval(calc)))
+    print(decore)
+    mcalcreq()
 
 
 def console():
-    calculator_mode = input("Select Calculator Mode: ")
-    if calculator_mode == "help":
+    calcmode = input("Select Calculator Mode: ")
+    if calcmode == "help":
         print(decore)
-        print('square root mode: "sqrt"')
-        print('normal Calculator Mode: "calc"')
-        print('console version: "version"')
+        print('normal Calculator Mode             : "calc"')
+        print('Multiple operations calculator mode: "mcalc"')
+        print('square root mode                   : "sqrt"')
+        print('console version                    : "version"')
+        print('Project Links                      : "projectlinks"')
         print(decore)
         console()
-    elif calculator_mode == "calc":
+    elif calcmode == "calc":
         calc()
-    elif calculator_mode == "sqrt":
-        squareroot()
-    elif calculator_mode == "version":
-        print("pycalc console v1.0 build 062621")
+    elif calcmode == "sqrt":
+        sqrt()
+    elif calcmode == "version":
+        print("pycalc console v1.3 build 112821")
         console()
-    elif calculator_mode == "exit":
-        time.sleep(0.5)
+    elif calcmode == "exit":
         print(byetext)
-        time.sleep(0.5)
         sys.exit()
+    elif calcmode == "projectlinks":
+        print("Github: https://www.github.com/techvio1/PyCalc-Project/")
+        print("GitLab: not ready yet")
+        console()
+    elif calcmode == "mcalc":
+        mcalc()
     else:
         print(decore)
         print("Incorrect mode: Type 'help' for more information")
