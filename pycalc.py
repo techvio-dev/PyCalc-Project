11599a85798fee0625160cd2c54b9f17e10e9d45
TechVio <68242352+techvio1@users.noreply.github.com>
2022-02-19 21:14:08 +0100

    Pycalc 7.0 release

--- pycalc.py
+++ pycalc.py
@@ -1,116 +1,100 @@
 import sys
 decore = "--------------------------------------"
-byetext = "\n\nThank you For Using Our Program\n\nExiting..."
-print(decore + "\nWelcome to PyCalc project: CLI Edition\n" + decore)
-print("Version: 7.0\n" + "Branch : Unstable pre-release\n" + decore)
-print("Copyright © 2022 TechVio\n" + decore + "\nLicensed under MIT license\n" + decore)
-print("Starting PyCalc console...\n" + decore)
+qtxt = "\n\nThank you For Using Our Program\nExiting..."
+pccv = "V2.0 build 021922"
+reqtxt = "Do you want to do another operation (y/n/q): "
+btc = "\nBack to the console\n"
+badparam = "\nIncorrect parameter, back to console\n"
+print(decore+"\nWelcome to PyCalc project: CLI Edition\n"+decore+"\nVersion: 7.0\n"+"Branch: Unstable\n"+decore+"\nCopyright © 2022 TechVio\n"+decore+"\nLicensed under MIT license\n"+decore+"\nPyCalc console "+pccv+"\n"+decore)
 
 
 def calcreq():
-    req = str.lower(input("Do you want to do another operation (y/n/q): "))
+    req = str.lower(input(reqtxt))
     match req:
-        case "y":
-            print("please wait...")
+        case 'y':
             calc()
-        case "n":
-            print(decore + "\nBack to console\n" + decore)
+        case 'n':
+            print(decore + btc + decore)
             console()
-        case "q":
-            print(byetext)
+        case 'q':
+            print(qtxt)
             sys.exit()
         case _:
-            print(decore)
-            print("Incorrect Parameter, back to console")
-            print(decore)
+            print(decore + badparam + decore)
             console()
 
 
 def mcalcreq():
-    req = str.lower(input("Do you want to do another operation (y/n/q): "))
+    req = str.lower(input(reqtxt))
     match req:
-        case "y":
-            print("please wait...")
+        case 'y':
             mcalc()
-        case "n":
-            print(decore + "\nBack to console\n" + decore)
+        case 'n':
+            print(decore+btc+decore)
             console()
-        case "q":
-            print(byetext)
+        case 'q':
+            print(qtxt)
             sys.exit()
         case _:
-            print(decore)
-            print("Incorrect Parameter, back to console")
-            print(decore)
+            print(decore+badparam+decore)
             console()
 
 
 def sqrtreq():
-    req = str.lower(input("Do you want to do another operation (y/n/q): "))
+    req = str.lower(input(reqtxt))
     match req:
-        case "y":
-            print("please wait...")
+        case 'y':
             sqrt()
-        case "n":
-            print(decore + "\nBack to console\n" + decore)
+        case 'n':
+            print(decore + btc + decore)
             console()
-        case "q":
-            print(byetext)
+        case 'q':
+            print(qtxt)
             sys.exit()
         case _:
-            print(decore)
-            print("Incorrect Parameter, back to console")
-            print(decore)
+            print(decore + badparam + decore)
             console()
 
 
 def calc():
-
     print(decore)
-    num1 = float(input("Enter Your First Number: "))
+    n1 = float(input("Enter Your First Number: "))
     print(decore)
     op = input("Enter Your Operation: ")
     print(decore)
-    num2 = float(input("Enter Your Second Number:"))
+    n2 = float(input("Enter Your Second Number:"))
     print(decore)
     match op:
         case "*":
-            result = float(num1 * num2)
+            result = float(n1*n2)
             print(result)
             calcreq()
         case "+":
-            result = float(num1 + num2)
+            result = float(n1+n2)
             print(result)
             calcreq()
         case "-":
-            result = float(num1 - num2)
+            result = float(n1-n2)
             print(result)
             calcreq()
         case "/":
-            result = float(num1 / num2)
+            result = float(n1/n2)
             print(result)
             calcreq()
         case "%":
-            result = float(num1 % num2)
+            result = float(n1 % n2)
             print(result)
             calcreq()
         case "**":
-            result = float(num1 ** num2)
+            result = float(n1**n2)
             print(result)
             calcreq()
         case "//":
-            result = float(num1 // num2)
+            result = float(n1//n2)
             print(result)
             calcreq()
         case _:
-            print("Error: Incorrect Operation:")
-            print("Addition      : +")
-            print("Subtraction   : -")
-            print("Multiplication: *")
-            print("Division      : /")
-            print("Modulus       : %")
-            print("Exponentiation: **")
-            print("Floor division: //")
+            print("Error: Incorrect operation:"+"\nAddition      : +"+"\nSubtraction   : -"+"\nMultiplication: *"+"\nDivision      : /"+"\nModulus       : %"+"\nExponentiation: **"+"\nFloor division: //")
             calc()
 
 
@@ -126,8 +110,8 @@ def sqrt():
 
 
 def mcalc():
-    calc = input("Type calculation: ")
-    print(str(eval(calc)))
+    calcul = input("Type calculation: ")
+    print(str(eval(calcul)))
     print(decore)
     mcalcreq()
 
@@ -136,13 +120,7 @@ def console():
     calcmode = str.lower(input("Select Calculator Mode: "))
     match calcmode:
         case "help":
-            print(decore)
-            print('normal Calculator Mode             : "calc"')
-            print('Multiple operations calculator mode: "mcalc"')
-            print('square root mode                   : "sqrt"')
-            print('console version                    : "version"')
-            print('Project Links                      : "projectlinks"')
-            print(decore)
+            print(decore+'\nNormal calculator mode             : "calc"'+'\nMulti-operations calculator mode: "mcalc"'+'\nsquare root mode                   : "sqrt"'+'\nConsole version                    : "version"'+'\nProject links                      : "projectlinks"\n'+decore)
             console()
         case "calc":
             calc()
@@ -151,19 +129,16 @@ def console():
         case "sqrt":
             sqrt()
         case "version":
-            print("pycalc console v2.0 build 021922")
+            print(pccv)
             console()
         case "exit":
-            print(byetext)
+            print(qtxt)
             sys.exit()
         case "projectlinks":
-            print("Github: https://www.github.com/techvio1/PyCalc-Project/")
-            print("GitLab: Working on final touches")
+            print("Github: https://www.github.com/techvio1/PyCalc-Project/"+"\nGitLab: Working on final touches")
             console()
         case _:
-            print(decore)
-            print("Incorrect mode: Type 'help' for more information")
-            print(decore)
+            print(decore+"\nIncorrect mode: Type 'help' for more informations\n"+decore)
             console()
 
 
